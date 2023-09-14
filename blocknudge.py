68a68f217eac914425cc401d68054f5da998d230
dmartin5150 <dmartin5150@icloud.com>
2023-09-14 13:04:11 -0500

    finished function for monthly block use

--- blocknudge.py
+++ blocknudge.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 units = ['BH JRI','STM ST OR', 'MT OR','BH CSC','ST OR']
-months = ['6','7','8']
+months = ['6','7','8','9']
 
 
 
@@ -33,30 +33,43 @@ all_npis = all_blocks['NPI']
 # print(all_blocks)
 
 def checkValidNPI(npi, row):
-    # print('row', row['npis'])
     if (row['npis'] == '[]'):
         return False
-    # print('row', row)
-    # print('pre')
     curNpi = row['npis'].strip('[,]')
-    # print('post')
     if (str(npi) == curNpi):
         return True
     return False
-curBlocks = pd.DataFrame()
-for unit in units:
-    for month in months:
-        curFile = month + "_2023_" + unit + ".csv"
-        curdata = pd.read_csv(curFile)
-        curdata = curdata[(curdata['blockType'] == 'Surgeon') & (curdata['type'] == 'ALL')]
-        for npi in all_npis:
+
+def getMonthlyBlockData(units, months, surgeon_group):
+    block_owners = pd.read_csv('block_id_owners.csv')
+    blocks_with_owners = block_owners.merge(surgeon_group, how='inner', left_on='npi', right_on='NPI')
+    all_npis = blocks_with_owners['NPI']
+    curBlocks = pd.DataFrame()
+    for unit in units:
+        for month in months:
+            curFile = month + "_2023_" + unit + ".csv"
             curdata = pd.read_csv(curFile)
             curdata = curdata[(curdata['blockType'] == 'Surgeon') & (curdata['type'] == 'ALL')]
-            # print('npi', curdata['npis'])
-            curdata['inBlock'] = curdata.apply(lambda row: checkValidNPI(npi, row), axis=1)
-            curdata['npi'] = npi
-            curdata = curdata[curdata['inBlock'] == True]
-            curdata.drop(['inBlock','npis'], axis=1,inplace=True)
-            curBlocks= curBlocks.append(curdata)
-            # print(unit, curFile, npi, curdata.shape)
-print(curBlocks)
+            for npi in all_npis:
+                npiData = curdata.copy()
+                npiData['inBlock'] = npiData.apply(lambda row: checkValidNPI(npi, row), axis=1)
+                npiData['npi'] = npi
+                npiData = npiData[npiData['inBlock'] == True]
+                if npiData.empty:
+                    continue
+                bt_minutes = npiData['bt_minutes'].sum()
+                nbt_minutes = npiData['nbt_minutes'].sum()
+                total_minutes = npiData['total_minutes'].sum()
+                flexId = npiData.iloc[0]['id']
+                utilization = 0
+                if (total_minutes != 0):
+                    utilization = str(round(bt_minutes/total_minutes * 100,1)) +'%'
+                npiData.drop(['inBlock','npis'], axis=1,inplace=True)
+                curBlocks= curBlocks.append({'flexId':flexId ,'unit':unit,'month':month, 'npi': npi,'bt_minutes':bt_minutes,'nbt_minutes':nbt_minutes, 'total_minute':total_minutes, 'utilization':utilization}, ignore_index=True)
+                # print(unit, curFile, npi, curdata.shape)
+    curBlocks=curBlocks.merge(all_blocks,  left_on='npi', right_on='NPI')
+
+    curBlocks.drop(['npis', 'npi_x','npi_x', 'id', 'blockType'], axis=1, inplace=True)
+    print(curBlocks.sort_values(by=['unit', 'NPI', 'month']))
+
+getMonthlyBlockData(units, months, toa)
