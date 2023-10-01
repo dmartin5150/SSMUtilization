9dccbc558810b27142b215bbbbb67431da3abdc5
Martin <david.martin6@ascension.org>
2023-10-01 13:24:08 -0500

    first commit

--- block.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-from facilityconstants import units
-     
-print(units)
-
--- testcscdata.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from datetime import datetime
-from utilities import get_procedure_date, create_zulu_datetime_from_string,get_block_date_with_timezone
-
-curFile = pd.read_csv('csc_gen_data.csv')
-print(curFile[(curFile['procedureDtNoTime'] == '2023-09-27') & (curFile['room'] == 'BH CSC 09')])
-
-# print(curFile[(curFile['proc_date']== '2023-09-27') & (curFile['room']== 'BH CSC 09')])
-
